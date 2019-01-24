60afe9f547f8d2da8f352d01a19ea0027f70ed8d
taokaenoi01 <46810263+taokaenoi01@users.noreply.github.com>
2019-01-24 16:07:16 +0700

    Update app2.js

--- app2.js
+++ app2.js
@@ -11,7 +11,7 @@ app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     let userid = req.body.events[0].source.userId
-    reply(reply_token, msg)
+    reply(reply_token, msg, userid)
     res.sendStatus(200)
 })
 app.listen(port)
